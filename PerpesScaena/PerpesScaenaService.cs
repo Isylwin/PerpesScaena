d74b9b1a58950ab0f8719cfa1a503f628e0bcc13
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-10-06 23:21:36 +0200

    Initial commit

---
+++ PerpesScaena/PerpesScaenaService.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace PerpesScaena
+{
+    public partial class PerpesScaenaService : ServiceBase
+    {
+        private readonly System.Diagnostics.EventLog _eventLogger;
+
+        private const int RefreshRate = 60*60*1000;
+
+        [DllImport("user32.dll")]
+        public static extern int SystemParametersInfo(
+               uint action, uint uParam, string vParam, uint winIni);
+
+        public static readonly uint SpiSetdeskwallpaper = 0x14;
+        public static readonly uint SpifUpdateinifile = 0x01;
+        public static readonly uint SpifSendwininichange = 0x02;
+
+        public PerpesScaenaService()
+        {
+            InitializeComponent();
+            _eventLogger = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("PerpesScaenaSource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "PerpesScaenaSource", "PerpesScaenaLog");
+            }
+            _eventLogger.Source = "PerpesScaenaSource";
+            _eventLogger.Log = "PerpesScaenaLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _eventLogger.WriteEntry("Starting up.");
+            var timer = new Timer {Interval = RefreshRate};
+            timer.Elapsed += OnTimer;
+            timer.Start();
+            OnTimer(null, null);
+        }
+
+        private void OnTimer(object sender, ElapsedEventArgs e)
+        {
+            _eventLogger.WriteEntry("Changing background image.");
+            SystemParametersInfo(SpiSetdeskwallpaper, 0, @"C:/image.bmp",
+                    SpifUpdateinifile | SpifSendwininichange);
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
--- PerpesScaena/Program.cs
+++ PerpesScaena/Program.cs
@@ -17,7 +17,7 @@ namespace PerpesScaena
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
             {
-                new Service1()
+                new PerpesScaenaService()
             };
             ServiceBase.Run(ServicesToRun);
         }
